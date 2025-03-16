eb3341b1e89a4cebc9a663b1c9f0f88d54f3ab04
Keval2005 <vadoliyakeval@gmail.com>
2025-03-16 21:17:17 +0530

    Stack is another way to implement layout & navigation within react-native, & nativeWind is Used to use TailwindCSS in React-Native

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>jati reje!!!!!!!!</Text>
       <StatusBar style="auto" />
+      
+      <Link href="/profile">Go to profile</Link>
+      {/* <Link> is for going to one page to another */}
+
     </View>
   );
 }
---
+++ app/profile.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default profile
+const styles = StyleSheet.create({})
