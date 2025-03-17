d63950be852ac07053c162e04a3dcfe372bf8324
Keval2005 <vadoliyakeval@gmail.com>
2025-03-17 16:58:33 +0530

    created organised Folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import {  Text, View } from "react-native";
-import { Link } from "expo-router";
+import { SplashScreen,Link } from "expo-router";
 import { verifyInstallation } from 'nativewind';
 import { NativeWindStyleSheet } from "nativewind";
+import { useFonts } from "expo-font";
 import '../global.css';
+import { use, useEffect } from "react";
 
 export default function App() {
   
@@ -12,6 +14,8 @@ export default function App() {
   // NativeWindStyleSheet.setOutput({
   //   default: "native",
   // });
+
+  
 
   return (
     <View className="flex-1 text-center justify-center items-center bg-gray-100">
@@ -22,13 +26,13 @@ export default function App() {
       <Text className="text-3xl">jati reje!!!!!!!!</Text> */}
 
         
-        <Text className="text-3xl text-red-200">jati reje!!!!!!!!</Text>
+        <Text className="text-3xl text-blue-500 font-pblack">jati reje!!!!!!!!</Text>
         <Text className="text-3xl text-red-300">jati reje!!!!!!!!</Text>
         <Text className="text-3xl text-red-400">jati reje!!!!!!!!</Text>
         <Text className="text-3xl text-red-500">jati reje!!!!!!!!</Text>
       <StatusBar style="auto" />
       
-      <Link href="/profile">Go to profile</Link>
+      <Link className="text-3xl  font-pblack" href="/profile">Go to profile</Link>
       {/* <Link> is for going to one page to another */}
 
     </View>
