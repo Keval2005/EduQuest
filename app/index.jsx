719f837599417bcc16ab7ae365735a4ab894440c
Keval2005 <vadoliyakeval@gmail.com>
2025-03-17 16:07:57 +0530

    Tailwind CSS working now with successful nativewind installation...

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import {  Text, View } from "react-native";
 import { Link } from "expo-router";
-import "./global.css";
+import { verifyInstallation } from 'nativewind';
+import { NativeWindStyleSheet } from "nativewind";
+import '../global.css';
 
 export default function App() {
+  
+  // verifyInstallation();
+
+  // NativeWindStyleSheet.setOutput({
+  //   default: "native",
+  // });
+
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 text-center justify-center items-center bg-gray-100">
+      {/* <Text className="text-3xl">jati reje!!!!!!!!</Text>
       <Text className="text-3xl">jati reje!!!!!!!!</Text>
+      <Text className="text-3xl">jati reje!!!!!!!!</Text>
+      <Text className="text-3xl">jati reje!!!!!!!!</Text>
+      <Text className="text-3xl">jati reje!!!!!!!!</Text> */}
+
+        
+        <Text className="text-3xl text-red-200">jati reje!!!!!!!!</Text>
+        <Text className="text-3xl text-red-300">jati reje!!!!!!!!</Text>
+        <Text className="text-3xl text-red-400">jati reje!!!!!!!!</Text>
+        <Text className="text-3xl text-red-500">jati reje!!!!!!!!</Text>
       <StatusBar style="auto" />
       
       <Link href="/profile">Go to profile</Link>
