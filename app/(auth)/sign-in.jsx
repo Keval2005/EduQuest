023e0b12a3fdc3fb538678a4f792fdf9adf2a2dd
Keval2005 <vadoliyakeval@gmail.com>
2025-04-10 16:52:51 +0530

    User-login saved on device

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -35,7 +35,7 @@ const SignIn = () => {
             setUser(result);
             setIsLogged(true);
             
-            // Alert.alert("Success", "User signed in successfully");
+            Alert.alert("Success", "User signed in successfully");
             router.replace("/home");
             
         }catch(error){
--- app/index.jsx
+++ app/index.jsx
@@ -12,9 +12,9 @@ import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
   
-  const { isLoading, isLoggedIn  } = useGlobalContext();
+  const { loading, isLogged  } = useGlobalContext();
 
-  if(!isLoading && isLoggedIn) return <Redirect href="/home" />;
+  if(!loading && isLogged) return <Redirect href="/home" />;
   // verifyInstallation();
 
   // NativeWindStyleSheet.setOutput({
