f3c03f2ab7481e811546ef7c4290ae08724d55b0
Keval2005 <vadoliyakeval@gmail.com>
2025-04-12 13:41:42 +0530

    Profile Section Created Successfully

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -7,16 +7,15 @@ import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
 import { useState } from 'react'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'  
-
 import VideoCard from '../../components/VideoCard'
-
 import useAppwrite from '../../lib/useAppwrite'
-import { use } from 'react'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
 
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
+  const { user, setUser, setIsLogged } = useGlobalContext();
 
   const [refreshing, setRefreshing] = useState(false)
 
@@ -38,8 +37,8 @@ const Home = () => {
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-                <Text className="text-2xl font-psemibold text-white">Ukt</Text>
+                <Text className="font-pmedium text-sm text-gray-100">Welcome Back,</Text>
+                <Text className="text-2xl font-psemibold text-white">{user?.username}</Text>
               </View>
               <View className="mt-1.5">
               <Image
